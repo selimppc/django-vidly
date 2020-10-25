e4c357939a135822f2f59a29ff0efe3447c18723
Selim Reza <selim.reza@bongobd.com>
2020-10-25 11:19:28 +0600

    changed version 2.1.5

--- vidly/settings.py
+++ vidly/settings.py
@@ -40,8 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'movies.apps.MoviesConfig',
-    'api.apps.ApiConfig'
+    'movies',
+    'api'
 ]
 
 MIDDLEWARE = [
